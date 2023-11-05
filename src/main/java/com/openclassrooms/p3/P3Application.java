c4e66a76f04e9951838ff7607777427c62dfab06
Younes Lahouiti <loller763@gmail.com>
2023-11-05 10:31:38 +0100

    Initialization of the Java project

---
+++ src/main/java/com/openclassrooms/p3/P3Application.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.p3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/openclassrooms/p3/P3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.p3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
