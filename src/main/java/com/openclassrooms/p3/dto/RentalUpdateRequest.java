e29e8157a20ac9acb9a0d8375e988bad1019bd80
Younes Lahouiti <loller763@gmail.com>
2023-11-11 14:56:01 +0100

    Addition of the fields of the submitted form in the DTO class for the request

--- src/main/java/com/openclassrooms/p3/dto/RentalUpdateRequest.java
+++ src/main/java/com/openclassrooms/p3/dto/RentalUpdateRequest.java
@@ -1,8 +1,16 @@
 package com.openclassrooms.p3.dto;
 
+import org.springframework.web.multipart.MultipartFile;
 import lombok.Value;
 
+/**
+ * Request payload containing the Form Data for updating a rental.
+ */
 @Value
 public class RentalUpdateRequest {
-
+    private String name;
+    private Integer surface;
+    private Double price;
+    private MultipartFile picture;
+    private String description;
 }
