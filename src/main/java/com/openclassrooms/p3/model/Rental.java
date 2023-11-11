636616001fbc768a69f01469e4b643ec4e52f7cf
Younes Lahouiti <loller763@gmail.com>
2023-11-11 10:58:42 +0100

    Fixed the placement of the model files

--- src/main/java/com/openclassrooms/p3/model/Rental.java
+++ src/main/java/com/openclassrooms/p3/model/Rental.java
@@ -1,4 +1,4 @@
-package com.openclassrooms.p3.repository;
+package com.openclassrooms.p3.model;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
--- src/main/java/com/openclassrooms/p3/model/User.java
+++ src/main/java/com/openclassrooms/p3/model/User.java
@@ -1,4 +1,4 @@
-package com.openclassrooms.p3.repository;
+package com.openclassrooms.p3.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
