7bb660f6edb4db35fde589da1a275ce787d76d9a
Younes Lahouiti <loller763@gmail.com>
2023-12-03 10:44:06 +0100

    Change of location for the test files

--- src/test/java/com/openclassrooms/p3/controller/RentalControllerTest.java
+++ src/test/java/com/openclassrooms/p3/controller/RentalControllerTest.java
@@ -1,10 +1,9 @@
-package com.openclassrooms.p3;
+package com.openclassrooms.p3.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.openclassrooms.p3.utils.JwtUtil;
